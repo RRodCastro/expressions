04ae27540d8fc61c76b0ac9e9464313e24d74fbe
Rrodcastro <mrcastro9574@gmail.com>
2018-09-05 18:26:49 -0600

    Calculate distances with scipy

--- frames-to-landmarks/distances_from_landmarks.py
+++ frames-to-landmarks/distances_from_landmarks.py
@@ -1,5 +1,6 @@
 from imutils.video import FileVideoStream
 from imutils import face_utils
+from scipy.spatial import distance as dist
 import numpy as np
 import argparse
 import imutils
@@ -8,6 +9,7 @@ import dlib
 import cv2
 import sys
 from time import sleep
+from os import listdir
 
 LANDMARKS = ['mouth', 'right_eyebrow', 'left_eyebrow', 'right_eye', 'left_eye']
 
@@ -23,6 +25,7 @@ predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
 time.sleep(1.0)
 
+
 def getLandmarks(data, facial_landmarks, sex, truth):
     mouth = facial_landmarks[LANDMARKS[0]]
     right_eyebrow = facial_landmarks[LANDMARKS[1]]
@@ -30,11 +33,6 @@ def getLandmarks(data, facial_landmarks, sex, truth):
     right_eye = facial_landmarks[LANDMARKS[3]]
     left_eye = facial_landmarks[LANDMARKS[4]]
 
-    # mouth = np.array(ast.literal_eval(mouth))
-    # right_eyebrow = np.array(ast.literal_eval(right_eyebrow))
-    # left_eyebrow = np.array(ast.literal_eval(left_eyebrow))
-    # right_eye = np.array(ast.literal_eval(right_eye))
-    # left_eye = np.array(ast.literal_eval(left_eye))
     center_rigth_eye = (
         right_eye[1][0] + ((right_eye[2][0] - right_eye[1][0]) / 2))
 
@@ -42,35 +40,43 @@ def getLandmarks(data, facial_landmarks, sex, truth):
                        ((left_eye[2][0] - left_eye[1][0])/2))
     unit = (center_left_eye - center_rigth_eye) / 64
 
-    lips_width = abs(mouth[6][0] - mouth[0][0])
-    lips_height = (abs(mouth[2][1] - mouth[10][1]) +
-                   abs(mouth[4][1] - mouth[8][1]))/2
+    lips_width = dist.euclidean(mouth[6], mouth[0])
 
-    inner_mouth = (abs(mouth[19][1] - mouth[13][1]) + abs(mouth[18]
-                                                          [1] - mouth[14][1]) + abs(mouth[17][1] - mouth[15][1]))/3
+    lips_height = (dist.euclidean(mouth[2], mouth[10]) +
+                   dist.euclidean(mouth[4], mouth[8]))/2
 
-    rigth_eyebrown_eye_distance = abs(
-        right_eye[3][1] - right_eyebrow[4][1])
-    left_eyebrown_eye_distance = abs(left_eye[0][1] - left_eyebrow[0][1])
+    inner_mouth = (dist.euclidean(mouth[19], mouth[13])
+                   + dist.euclidean(mouth[17], mouth[15]))/2
 
-    data.write(str(rigth_eyebrown_eye_distance * unit) + '-' + str(left_eyebrown_eye_distance *
-                                                                                                      unit) + '-' + str(lips_width * unit) + '-' + str(lips_height * unit) + '-' + str(inner_mouth * unit) + '-' + sex + '-' + str(truth) + '\n')
+    rigth_eyebrown_eye_distance = (dist.euclidean(
+        right_eye[1], right_eyebrow[2]) + dist.euclidean(right_eye[2], right_eyebrow[3]))/2
+
+    left_eyebrown_eye_distance = (dist.euclidean(
+        left_eye[1], left_eyebrow[1]) + dist.euclidean(left_eye[2], left_eyebrow[2]))/2
+
+    data.write(str(rigth_eyebrown_eye_distance * unit) + '-' +
+               str(left_eyebrown_eye_distance * unit) + '-' +
+               str(lips_width * unit) + '-' +
+               str(lips_height * unit) + '-' +
+               str(inner_mouth * unit) + '-' +
+               str(sex) + '-' + str(truth) + '\n')
+
 
 def detect_face_parts(detector, predictor, filePath, sex):
-    if ('V' in fileName):
+    if ('V' in filePath):
         truth = 1
     else:
         truth = 0
-    data = open("data.csv", "a")
+    data = open("data2.csv", "a")
 
     stream = cv2.VideoCapture(filePath)
     while True:
-    # if fileStream and not vs.more():
-    #     break 
-    #skip 5 frames
-        for i in range(5):
+        # if fileStream and not vs.more():
+        #     break
+        # skip 5 frames
+        for i in range(3):
             stream.grab()
-            (grabbed, frame) = stream.read()
+        (grabbed, frame) = stream.read()
         if not grabbed:
             return
 
@@ -85,21 +91,25 @@ def detect_face_parts(detector, predictor, filePath, sex):
                 if name in LANDMARKS:
                     # for (x, y) in shape[i:j]:
                     facial_landmarks[name] = shape[i:j]
-                        # cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
-            getLandmarks(data, facial_landmarks, sex, truth)
+                    # cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
             # cv2.imshow("MP", frame)
+            getLandmarks(data, facial_landmarks, sex, truth)
             # key = cv2.waitKey(1) & 0xFF
 
             # if the `q` key was pressed, break from the loop
         # if key == ord("q"):
         #     break
-    
+
     data.close()
     # cv2.destroyAllWindows()
 
-fileName = sys.argv[1] if len(sys.argv) > 1 else ""
-sex = sys.argv[2]
 
-filePath = "../../videos/%s.mp4" % fileName
+# fileName = sys.argv[1] if len(sys.argv) > 1 else ""
+# sex = sys.argv[2]
+sex = 1
+videos = listdir("../../videos/")
 
-detect_face_parts(detector, predictor, filePath, sex)
+for video in videos:
+    filePath = "../../videos%s" % video
+    print(video)
+    detect_face_parts(detector, predictor, filePath, sex)
