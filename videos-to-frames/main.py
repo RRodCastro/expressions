34b1d39d377f4fec2f86c6e505929b6ac52b7546
Rrodcastro <mrcastro9574@gmail.com>
2018-10-14 19:52:37 -0600

    Script to transform video to frames (with responsens times)

--- videos-to-frames/main.py
+++ videos-to-frames/main.py
@@ -13,10 +13,10 @@ import shutil
 
 def videoStreamer(path, width=600, height=600, skip=None):
     # capture the video
-    filePath = "../../videos/%s.mp4" % path
+    filePath = "../../videos/done/%s.mp4" % path
     stream = cv2.VideoCapture(filePath)
-    timesPath = "../../times/%s.csv" % path
-    times = open(timesPath, 'r')
+    timesPath = "../../times/done/%s.csv" % path
+    times = open(timesPath, 'rt')
     arrTimes = []
     for i in times:
         arrTimes.append(i.split(","))
@@ -32,6 +32,7 @@ def videoStreamer(path, width=600, height=600, skip=None):
     second = 0
     numberQuest = 1
     searchTime = arrTimes.pop(0)
+
     while True:
         # skips some some of the frames, and read one
         if(second > int(searchTime[2].replace('\n', ''))):
@@ -44,7 +45,7 @@ def videoStreamer(path, width=600, height=600, skip=None):
         (grabbed, frame) = stream.read()
         if not grabbed:
             return
-        if((index % 3 == 0) & (int(searchTime[1]) <= second <= int(searchTime[2].replace('\n', '')))):
+        if((index % 3 == 0) & (int(searchTime[1].replace("ï»¿", "")) <= second <= int(searchTime[2].replace("\n", "")))):
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             namedFrame = str(index) + "-" + \
                 str(numberQuest) + "-" + str(second)
@@ -54,13 +55,14 @@ def videoStreamer(path, width=600, height=600, skip=None):
         second = int(index/FPS)
 
 
-files = ['E015M', 'E015V', 'E016M', 'E016V', 'E018M', 'E018V', 'E027M', 'E027V', 'E030M', 'E030V',
-         'E031M', 'E031V', 'E033M', 'E033V', 'E034M', 'E034V', 'E036M', 'E036V', 'E037M', 'E037V', 'E038M']
-files2 = ['E038V', 'E039M', 'E039V', 'E042M', 'E042V', 'E045M', 'E045V', 'E046M', 'E046V', 'E048M', 'E048V',
-          'E052M', 'E052V', 'E054M', 'E054V', 'E060M', 'E060V', 'E062M', 'E062V', 'E065M', 'E065V', 'E066M', 'E067M']
 # fileName = sys.argv[1] if len(sys.argv) > 1 else ""
-for fileName in files2:
-    filePath = "../../videos/%s.mp4" % fileName
+
+H_REST = ['E006M', 'E006V', 'E010M', 'E010V',
+          'E036M', 'E036V', 'E064M', 'E068M']
+
+for fileName in H_REST:
+
+    filePath = "../../videos/done/%s.mp4" % fileName
     print(fileName)
     if (os.path.exists(filePath)):
         try:
